e0b771374a97c5e2f07a5ab871d28f416d267f79
Felipe Quiros <quirosfelipe@gmail.com>
2020-07-29 18:08:51 -0400

    deploy fix

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withCSS = require("@zeit/next-css"); // allows for the use of next css
+const { processEnv } = require("next/dist/lib/load-env-config");
+
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    APP_NAME: "BRANA YOGA",
+    API_DEVELOPMENT: "http://localhost:8000/api",
+    API_PRODUCTION: "https://branayoga.herokuapp.com/api",
+    PRODUCTION: true,
+    DOMAIN_DEVELOPMENT: "http://localhost:3000",
+    DOMAIN_PRODUCTION: "https://branayoga.herokuapp.com",
+    FB_APP_ID: process.env.FB_APP_ID,
+    DISQUS_SHORTNAME: process.env.DISQUS_SHORTNAME,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  },
+});
