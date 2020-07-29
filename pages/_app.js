46bae8583f4ae1e296351332c80427106b094b50
Felipe Quiros <quirosfelipe@gmail.com>
2020-07-29 17:53:09 -0400

    css fix2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import ".././node_modules/nprogress/nprogress.css";
-import "../../node_modules/react-quill/dist/quill.snow.css";
+import "../node_modules/nprogress/nprogress.css";
+import "../node_modules/react-quill/dist/quill.snow.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
