b08c1537ed2a9ad5faef7895634fb422497a8b0f
Felipe Quiros <quirosfelipe@gmail.com>
2020-08-03 16:41:27 -0400

    deploy fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withCSS({
     APP_NAME: "BRANA YOGA",
     API_DEVELOPMENT: "http://localhost:8000/api",
     API_PRODUCTION: "https://branayoga.herokuapp.com/api",
-    PRODUCTION: false,
+    PRODUCTION: true,
     DOMAIN_DEVELOPMENT: "http://localhost:3000",
     DOMAIN_PRODUCTION: "https://branayoga.herokuapp.com",
     FB_APP_ID: process.env.FB_APP_ID,
